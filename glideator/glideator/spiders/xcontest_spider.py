a19dac45d262e01d39bba91cd429525d31069f38
Jan Helcl <janhelcl@gmail.com>
2024-07-26 15:18:09 +0200

    simplify

--- glideator/glideator/spiders/xcontest_spider.py
+++ glideator/glideator/spiders/xcontest_spider.py
@@ -11,14 +11,13 @@ from scrapy_playwright.page import PageMethod
 class FlightsSpider(scrapy.Spider):
     name = 'flights'
     base_url = "https://www.xcontest.org/world/en/flights/daily-score-pg/"
-
     
-    def start_requests(self):
-        url = self.base_url + f'#filter[date]=2024-07-19@filter[country]=CZ@filter[detail_glider_catg]=FAI3'
-        yield scrapy.Request(
+    def get_response_spec(self, url, dont_filter):
+        return scrapy.Request(
             url=url,
             callback=self.parse,
             errback=self.errback_close_page,
+            dont_filter=dont_filter,
             meta={
                 "playwright": True,
                 "playwright_include_page": True,
@@ -32,6 +31,10 @@ class FlightsSpider(scrapy.Spider):
             }
         )
 
+    def start_requests(self):
+        url = self.base_url + f'#filter[date]=2024-07-19@filter[country]=CZ@filter[detail_glider_catg]=FAI3'
+        yield self.get_response_spec(url, False)
+
     async def parse(self, response):
         page = response.meta['playwright_page']
         content = await page.content()
@@ -41,61 +44,37 @@ class FlightsSpider(scrapy.Spider):
         table = selector.xpath('//table[contains(@class, "XClist")]')
         flights = table.xpath('.//tbody/tr')
         for flight in flights:
-            yield { 
-                'start_time': _parse_start_time(flight, date_str),
+            yield {
+                'date': date_str,
+                'start_time': _parse_start_time(flight),
                 'pilot': flight.xpath('./td[3]/div/a/b/text()').get().strip(),
                 'launch': flight.xpath('.//td[4]//div[@class="full"]/a/text()').get().strip(),
                 'type': flight.xpath('.//td[5]//text()').get().strip(),
                 'lenght': float(flight.xpath('.//td[6]//text()').get().strip()),
                 'points': float(flight.xpath('.//td[7]//text()').get().strip()),
-                # 'duration': _parse_duration(flight),
                 'glider_cat': flight.xpath('.//td[9]//text()').get().strip(),
-                'glider': flight.xpath('.//td[9]//div/@title').get().strip()
+                'glider': flight.xpath('.//td[9]//div/@title').get().strip(),
+                'flight_id': int(flight.xpath('./@id').get().strip().replace("flight-", "")),
             }
         next_href = response.xpath('//div[contains(@class, "XCpager")]//a[contains(@title, "next page")]/@href').get().strip()
         next_url = self.base_url + next_href
         if next_url != response.url:
-            yield scrapy.Request(
-                    url=next_url,
-                    callback=self.parse,
-                    errback=self.errback_close_page,
-                    dont_filter=True,
-                    meta={
-                        "playwright": True,
-                        "playwright_include_page": True,
-                        "playwright_context": "new",
-                        "playwright_context_kwargs": {
-                            "proxy": {"server": "http://35.185.196.38:3128"} # "http://163.172.33.137:4671" "http://35.185.196.38:3128"
-                        },
-                        'playwright_page_methods': [
-                            PageMethod('wait_for_timeout', 5000),
-                        ]
-                    }
-                )
-            
-        
+            yield self.get_response_spec(next_url, True)          
 
     async def errback_close_page(self, failure):
         page = failure.request.meta["playwright_page"]
         await page.close()
 
 
-def _parse_start_time(flight, date_str):
-    """
-    #TODO
-    """
-    time_part = flight.xpath('.//td[2]//div/em/text()').get().strip()
+def _parse_start_time(flight):
+    time_str = flight.xpath('.//td[2]//div/em/text()').get().strip()
     utc_offset = flight.xpath('.//td[2]//div/span[@class="XCutcOffset"]/text()').get().strip()
-    datetime_str = f"{date_str} {time_part}"
     tz = _parse_time_zone(utc_offset)
-    dt = datetime.strptime(datetime_str, "%Y-%m-%d %H:%M")
+    dt = datetime.strptime(time_str, "%H:%M").time()
     return dt.replace(tzinfo=tz)
 
 
 def _parse_time_zone(timezone_str):
-    """
-    #TODO
-    """
     timezone_str = timezone_str[1:]
     sign = timezone_str[3]
     hours = int(timezone_str[4:6])
@@ -105,15 +84,3 @@ def _parse_time_zone(timezone_str):
         total_minutes = -total_minutes
     offset = timedelta(minutes=total_minutes)
     return timezone(offset)
-
-
-def _parse_duration(flight):
-    """
-    #TODO
-    """
-    dur_str1 = flight.xpath('.//td[8]//span[@class="d0"]//text()').get()
-    dur_str2 = flight.xpath('.//td[8]//span[@class="d1"]//text()').get()
-    dur_str = dur_str1 + dur_str2
-    hours, minutes = dur_str.replace(" ", "").split(':')
-    return int(hours) * 60 + int(minutes)
-    
