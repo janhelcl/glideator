4038f6c6efa3b15633c997a1b9d07b235e121e6a
Jan Helcl <janhelcl@gmail.com>
2024-09-08 19:29:40 +0200

    extend launches

--- db/glideator/models/mart/dim_launches.sql
+++ db/glideator/models/mart/dim_launches.sql
@@ -8,11 +8,15 @@ launches AS (
 
 mapped_and_filtered AS (
 	SELECT
-		mapping.xcontest_name AS name,
-		launches.longitude,
-		launches.latitude,
-		launches.altitude,
-		launches.superelevation
+		xcontest_name AS name,
+		pgmap_name,
+		longitude,
+		latitude,
+		altitude,
+		superelevation,
+		ARRAY[wind_usable_from, wind_usable_to] AS usable_wind_range,
+		ARRAY[wind_optimal_from, wind_optimal_to] AS optimal_wind_range,
+		ROW_NUMBER() OVER (PARTITION BY xcontest_name ORDER BY pgmap_name) AS row_num
 	FROM launches
 	INNER JOIN mapping
 	ON launches.name = mapping.pgmap_name
@@ -21,10 +25,16 @@ mapped_and_filtered AS (
 aggregated AS (
 	SELECT
 		name,
-		MAX(longitude) AS longitude,
-		MAX(latitude) AS latitude,
+		AVG(longitude) AS longitude,
+		AVG(latitude) AS latitude,
 		MAX(altitude) AS altitude,
-		MAX(superelevation) AS superelevation
+		MAX(superelevation) AS superelevation,
+		MAX(CASE WHEN row_num = 1 THEN pgmap_name END) AS name1,
+		MAX(CASE WHEN row_num = 1 THEN usable_wind_range END) AS usable_wind_range1,
+		MAX(CASE WHEN row_num = 1 THEN optimal_wind_range END) AS optimal_wind_range1,
+		MAX(CASE WHEN row_num = 2 THEN pgmap_name END) AS name2,
+		MAX(CASE WHEN row_num = 2 THEN usable_wind_range END) AS usable_wind_range2,
+		MAX(CASE WHEN row_num = 2 THEN optimal_wind_range END) AS optimal_wind_range2
 	FROM mapped_and_filtered
 	GROUP BY 1
 )
