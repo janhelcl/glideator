96a94e35036ee55bf0a3b98e1e89e020dbec7aec
Jan Helcl <janhelcl@gmail.com>
2025-04-22 13:48:43 +0200

    Revert "force light mode"
    This reverts commit 1109eeaa2d9e8de809189c5b69dd8fe23556fdfe.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import Details from './pages/Details';
 import Declined from './pages/Declined';
 import NotFound from './pages/NotFound';
 
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-});
-
 const App = () => {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Router>
-        <Routes>
-          <Route path="/declined" element={<Declined />} />
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="details/:siteId" element={<Details />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </Router>
-    </ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path="/declined" element={<Declined />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="details/:siteId" element={<Details />} />
+          {/* Redirect old format URLs to new format */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
