9518afb4a047d6b9852614c0d615fd4145c31431
Jan Helcl <janhelcl@gmail.com>
2025-06-18 10:49:16 +0200

    add more marks

--- frontend/src/components/TripPlannerControls.jsx
+++ frontend/src/components/TripPlannerControls.jsx
@@ -281,7 +281,14 @@ const DistanceMetricControl = ({
   const marks = useMemo(() => [
     { value: 10, label: '10' },
     { value: 100, label: '100' },
+    { value: 200, label: '200' },
+    { value: 300, label: '300' },
+    { value: 400, label: '400' },
     { value: 500, label: '500' },
+    { value: 600, label: '600' },
+    { value: 700, label: '700' },
+    { value: 800, label: '800' },
+    { value: 900, label: '900' },
     { value: 1000, label: '1k' }
   ], []);
 
