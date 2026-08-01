7fb523874ca59fb803e5e3306cd393cf4d0ee924
Jan Helcl <janhelcl@gmail.com>
2026-08-01 15:47:11 +0200

    Add Vite browser entrypoint

---
+++ frontend/src/entry-client.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { createRoot, hydrateRoot } from 'react-dom/client';
+import { HydrationBoundary, QueryClientProvider } from '@tanstack/react-query';
+import { HelmetProvider } from 'react-helmet-async';
+import 'leaflet/dist/leaflet.css';
+import './index.css';
+
+import App from './App.jsx';
+import { queryClient } from './queryClient';
+
+const rootElement = document.getElementById('root');
+const dehydratedState = window.__REACT_QUERY_STATE__ || undefined;
+
+const application = (
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={dehydratedState}>
+        <HelmetProvider>
+          <App />
+        </HelmetProvider>
+      </HydrationBoundary>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, application);
+} else {
+  createRoot(rootElement).render(application);
+}
